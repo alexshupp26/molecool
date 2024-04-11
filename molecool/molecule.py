972e5852ecd10eef8b342ef0124b729cf6f7cdb8
Alexander Shupp <alexshupp26@vt.edu>
2024-04-11 09:28:11 -0400

    fixing imports

--- molecool/molecule.py
+++ molecool/molecule.py
@@ -1,67 +1,28 @@
-"""Provide the primary functions."""
+"""
+Functions for molecule analysis
+"""
+from .measure import calculate_distance
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-from mpl_toolkits.mplot3d import Axes3D ## noqa: F401
-
-
-def canvas(with_attribution=True):
+def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
     """
-    Placeholder function to show example docstring (NumPy format).
-
-    Replace this function and doc string for your own project.
+    Build a list of bonds in a set of coordinates based on a distance criteria.
 
     Parameters
     ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from.
+    coordinates: np.ndarray
+        The coordinates of the atoms to analyze in an (natoms, ndim) array.
+
+    max_bond: float, optional
+        The maximum distance for two atoms to be considered bonded.
+
+    min_bond: float, optional
+        The minimum distance for two atoms to be considered bonded.
 
     Returns
     -------
-    quote : str
-        Compiled string including quote and optional attribution.
+    bonds: dict
+        A dictionary containing bonded atoms with atom pairs as keys and the distance between the atoms as the value.
     """
-
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
-
-
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
-
-
-def zen(with_attribution=True):
-    quote = """Beautiful is better than ugly.
-    Explicit is better than implicit.
-    Simple is better than complex.
-    Complex is better than complicated.
-    Flat is better than nested.
-    Sparse is better than dense.
-    Readability counts.
-    Special cases aren't special enough to break the rules.
-    Although practicality beats purity.
-    Errors should never pass silently.
-    Unless explicitly silenced.
-    In the face of ambiguity, refuse the temptation to guess.
-    There should be one-- and preferably only one --obvious way to do it.
-    Although that way may not be obvious at first unless you're Dutch.
-    Now is better than never.
-    Although never is often better than *right* now.
-    If the implementation is hard to explain, it's a bad idea.
-    If the implementation is easy to explain, it may be a good idea.
-    Namespaces are one honking great idea -- let's do more of those!"""
-
-    if with_attribution:
-        quote += "\n\tTim Peters"
-
-    return quote
-
-
-def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
 
     # Find the bonds in a molecule (set of coordinates) based on distance criteria.
     bonds = {}
@@ -74,8 +35,3 @@ def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
                 bonds[(atom1, atom2)] = distance
 
     return bonds
-
-
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
